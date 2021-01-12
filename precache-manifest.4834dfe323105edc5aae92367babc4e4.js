5dfae0c1fefc90eec649d934a9691d16a9029017
Michał <michal.ciechanowicz@andoncloud.com>
2021-01-13 00:35:55 +0100

    Updates

---
+++ precache-manifest.4834dfe323105edc5aae92367babc4e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64019d1ada089056b4a89124d9d5808",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "39c2ccb0606b249df1be",
+    "url": "/covid19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "db86c19c5966e200b1dd",
+    "url": "/covid19-tracker/static/css/main.cedcec0d.chunk.css"
+  },
+  {
+    "revision": "39c2ccb0606b249df1be",
+    "url": "/covid19-tracker/static/js/2.ba68570c.chunk.js"
+  },
+  {
+    "revision": "931218552ec36a351f95bbf4f79fdeca",
+    "url": "/covid19-tracker/static/js/2.ba68570c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db86c19c5966e200b1dd",
+    "url": "/covid19-tracker/static/js/main.58a0ada2.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  },
+  {
+    "revision": "8f7675f43d3731de0cecec350e248bad",
+    "url": "/covid19-tracker/static/media/cov-19.8f7675f4.png"
+  }
+]);
